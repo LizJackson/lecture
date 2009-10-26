af8a4a0d29b91c2df316e3c2467cd5899b181543
LizJackson <liz.jackson@rice.edu>
2009-10-26 16:33:40 -0500

    changed name again

--- git-project.r
+++ git-project.r
@@ -3,16 +3,16 @@
 names <- read.csv("baby-names.csv", header = T, stringsAsFactors = F)
 
 # extracting all data related to my name
-my_name <- subset(names, name == "Elizabeth" & sex == "girl")
+elizabeth <- subset(names, name == "Elizabeth" & sex == "girl")
 delma <- subset(names, name == "Delma" & sex == "girl")
 
 # plotting my name's popularity over time
 library(ggplot2)
-qplot(year, prop, data = my_name, geom = "line", main = "Percentage of children named Elizabeth, by year")
+qplot(year, prop, data = elizabeth, geom = "line", main = "Percentage of children named Elizabeth, by year")
 
 
 # for later
-both_names <- rbind(my_name, second_name)
+both_names <- rbind(elizabeth, delma)
 
 qplot(year, prop, data = both_names, geom = "line", colour = name, main = "Popularity of group member names over time")
 ggsave("names.pdf", width = 5, height = 4)
